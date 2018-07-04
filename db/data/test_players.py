bf3535b384613066972c22ffd1aa91ac1bbe6029
Steve Journeay <sjournea@taralaur.com>
2018-07-04 15:55:19 -0700

    Added test data

---
+++ db/data/test_players.py
@@ -0,0 +1,14 @@
+"""test_players.py """
+DBGolfPlayers = (
+  {'email':'sjournea@tl.com', 'first_name': 'Steve', 'last_name': 'Journeay', 'nick_name': 'Hammy',  'handicap': 19.2, 'gender': 'man'},
+  {'email':'snake@tl.com',    'first_name': 'Chris', 'last_name': 'Jensen',   'nick_name': 'Snake',  'handicap': 18.0, 'gender': 'man'},
+  {'email':'spanky@tl.com',   'first_name': 'Rob',   'last_name': 'Sullivan', 'nick_name': 'Spanky', 'handicap': 16.5, 'gender': 'man'},
+  {'email':'reload@tl.com',   'first_name': 'Mike',  'last_name': 'Davis',    'nick_name': 'Reload', 'handicap': 22.9, 'gender': 'man'},
+  {'email':'bomba@tl.com',    'first_name': 'Rhonda','last_name': 'Journeay', 'nick_name': 'Bamba',  'handicap': 38.8, 'gender': 'woman'},
+  {'email':'ruby@tl.com',     'first_name': 'Ruby',  'last_name': 'Journeay', 'nick_name': 'Frog',   'handicap': 11.2, 'gender': 'woman'},
+  {'email':'perl@tl.com',     'first_name': 'Perl',  'last_name': 'Journeay', 'nick_name': 'Cattle', 'handicap':  8.6, 'gender': 'woman'},
+  {'email':'ricky@tl.com',    'first_name': 'Ricky', 'last_name': 'Rao',      'nick_name': 'Rao',    'handicap': 24.2, 'gender': 'man'},
+  {'email':'spike@tl.com',    'first_name': 'John',  'last_name': 'Molcillo', 'nick_name': 'Spike',  'handicap': 17.5, 'gender': 'man'},
+  {'email':'jimbo@tl.com',    'first_name': 'Jim',   'last_name': 'Nelson',   'nick_name': 'Jimbo',  'handicap': 10.2, 'gender': 'man'},
+)
+
